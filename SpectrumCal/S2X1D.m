c27ad232874ec09e265f1c575f37f7913b2b613b
xyy@ylp_db7 <xyy@ylp_db7>
2013-08-06 18:18:44 +0800

    S2X1D.m: fix bug: when error also return de

--- SpectrumCal/S2X1D.m
+++ SpectrumCal/S2X1D.m
@@ -4,6 +4,7 @@ function [X, de] = S2X1D(S)
   if (min(S)<=0) || iscomplex(S)
     warning('Error: can not factorize such spectrum!');
     X = NaN(size(S));
+    de= NaN;
     return;
   end
   X  = exp(hilbert(0.5*log(S)));
