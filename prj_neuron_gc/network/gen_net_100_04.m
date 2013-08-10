dc97f2284e9d67411dd8166368c9ab7fda865e62
xyy@ylp_db7 <xyy@ylp_db7>
2013-08-10 19:21:31 +0800

    add net_100_04.

---
+++ prj_neuron_gc/network/gen_net_100_04.m
@@ -0,0 +1,19 @@
+% generate group network from simle network
+
+netstr = 'net_100_04';
+
+rand('state',5);            % set initial state
+prob_one = 0.2;
+p=100;
+nnet = rand(p,p)<prob_one;
+nnet(eye(p)==1) = 0;
+
+nnet = nnet .* (1+randn(p)/5);
+nnet(nnet<=0) = 0;
+fd = fopen([netstr,'.txt'],'w');
+nnet = num2str(nnet);
+for jj=1:p
+  fprintf(fd, '%s\n', nnet(jj,:));
+end
+fclose(fd);
+
