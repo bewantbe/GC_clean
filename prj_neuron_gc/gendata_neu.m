abc875bb18caccad3130b933c0e1036e18f73d8e
xyy@ylp_db7 <xyy@ylp_db7>
2013-08-18 18:09:20 +0400

    now prj_neuron_gc/gendata_neu.m deal with Inhibitory neurons

--- prj_neuron_gc/gendata_neu.m
+++ prj_neuron_gc/gendata_neu.m
@@ -43,6 +43,22 @@ else
     else
         use_common_poisson = false;
     end
+    fp = strfind(extpara,'-n ');  % number of neurons
+    if (length(fp)>0)
+        fp = fp(end);  % use last occurrence
+        np = strfind(extpara(fp+3:end), ' ');
+        np = [np, length(extpara(fp+3:end))+1]
+        np([diff(np),2]<=1) = [] % delete continus white spaces
+        pE = str2num(extpara(fp+3:fp+1+np(1)));
+        if (length(np)>1)
+            pI = str2num(extpara(fp+2+np(1):fp+1+np(2)))
+        else
+            pI = 0;
+        end
+    else
+        pE = -1;
+        pI = -1;
+    end
 end
 
 pathdir = fileparts(mfilename('fullpath'));
@@ -54,8 +70,20 @@ else
     neu_network = load('-ascii', matname);
 end
 p = size(neu_network, 1);
-file_inf_st = sprintf('%s_sc=%g_pr=%g_ps=%g_t=%.2e_stv=%g',...
+if length(scee)==1 && pI==-1
+    file_inf_st = sprintf('%s_sc=%g_pr=%g_ps=%g_t=%.2e_stv=%g',...
                       netstr, scee, pr, ps, simu_time, stv);
+else
+    % the order of scee is: E2E, E2I, I2E, I2I
+    scee = [scee, zeros(1,4-length(scee))];   % extend to full form
+    st_scee = strrep(mat2str(scee),' ',',');
+    st_p = strrep(mat2str([pE,pI]),' ',',')
+    file_inf_st = sprintf('%s_p%s_sc=%s_pr=%g_ps=%g_t=%.2e_stv=%g',...
+                      netstr, st_p, st_scee, pr, ps, simu_time, stv);
+    if (pI+pE ~= p)
+        error('number of neurons does not match the network!');
+    end
+end
 file_prefix = [pwd(), '/data/'];   % force current dir
 if use_exp_IF
     file_prefix = [file_prefix, 'EIF_'];
@@ -71,17 +99,22 @@ if (exist(output_RAS_name, 'file') == 0 || new_run)
     end
 %    static_param = 'raster_tuning -ng -v --bin-save';     % if you are using M$ Windows
     if use_exp_IF
-        static_param = [pathdir, fs, 'raster_tuning_expIF -ng -v --bin-save -inf ', pathdir,'/test2.txt'];
+        static_param = [pathdir, fs, 'raster_tuning_expIF -ng -v --bin-save -inf -'];
     else
         if use_common_poisson
-            static_param = [pathdir, fs, 'raster_tuning_co -ng -v --bin-save -inf ', pathdir,'/test2.txt'];
+            static_param = [pathdir, fs, 'raster_tuning_co -ng -v --bin-save -inf -'];
         else
-            static_param = [pathdir, fs, 'raster_tuning -ng -v --bin-save -inf ', pathdir,'/test2.txt'];
+            static_param = [pathdir, fs, 'raster_tuning -ng -v --bin-save -inf -'];
         end
     end
-    cmdst = sprintf('%s -n %d -t %.16e -mat %s -pr %.16e -ps %.16e -scee %.16e --save-interval %.16e -o "%s" --save-spike-interval %s --save-spike %s', ...
-            static_param, p, simu_time, matname, pr, ps, scee, stv, output_name, output_ISI_name, output_RAS_name);
-%disp([cmdst, ' ', extpara]);  fflush(stdout);   % for debug
+    if length(scee)==1 && pI==-1
+        cmdst = sprintf('%s -n %d -t %.16e -mat %s -pr %.16e -ps %.16e -scee %.16e --save-interval %.16e -o "%s" --save-spike-interval "%s" --save-spike "%s"', ...
+                static_param, p, simu_time, matname, pr, ps, scee, stv, output_name, output_ISI_name, output_RAS_name);
+    else
+        cmdst = sprintf('%s -n %d %d -t %.16e -mat %s -pr %.16e -ps %.16e -scee %.16e -scei %.16e -scie %.16e -scii %.16e --save-interval %.16e -o "%s" --save-spike-interval "%s" --save-spike "%s"', ...
+                static_param, pE, pI, simu_time, matname, pr, ps, scee(1), scee(2), scee(3), scee(4), stv, output_name, output_ISI_name, output_RAS_name);
+    end
+disp([cmdst, ' ', extpara]);  fflush(stdout);   % for debug
     rt = system([cmdst, ' ', extpara]);
 else
     rt = 0;
