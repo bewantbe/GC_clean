73cc1947a102d251cf4a5abc68ba81702e00eb1f
xyy@ylp_db7 <xyy@ylp_db7>
2013-08-20 16:07:47 +0400

    add getnetwork for convenience

---
+++ prj_neuron_gc/getnetwork.m
@@ -0,0 +1,13 @@
+%
+
+function network = getnetwork(netstr);
+
+pathdir = fileparts(mfilename('fullpath'));
+if isempty(netstr)
+    matname = '-';
+    network = [1];
+else
+    matname = [pathdir, '/network/', netstr, '.txt'];
+    network = load('-ascii', matname);
+end
+
