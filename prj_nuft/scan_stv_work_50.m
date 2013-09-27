7c62a172bdb351b1bb033548a4a1ad13f57f65bd
xyy@xhp_Wheezy <xyy@xhp_Wheezy>
2013-09-27 14:56:16 +0800

    revert prj_nuft/scan_stv_work_50.m to correct version

--- prj_nuft/scan_stv_work_50.m
+++ prj_nuft/scan_stv_work_50.m
@@ -1,15 +1,15 @@
 %
 
 % _ST _expIF
-signature = 'data_scan_stv/IF_w51';
+signature = 'data_scan_stv/IF_w50';
 
 % scan value sets
 s_net  = {'net_2_2'};
-s_time = [1e7];
+s_time = [1e6];
 s_scee = [0.02];
 s_prps = [0.012];
 s_ps   = [0.012];
-s_stv  = [0.5:0.5:20];
+s_stv  = [0.5:0.5:1.5];
 maxod  = 99;
 s_od   = 1:maxod;
 hist_div = 0:0.5:400;         % ISI
