9a7926be741c71d045cc3ebc4b9f335d33c8fddc
xyy@dell-JM3Q <xyy@dell-JM3Q>
2013-12-08 19:16:26 +0800

    Now check whether the .mex files are up to date in startup.m, convert .octaverc to symbol link to startup.m since they are alway the same(at least so far). update install.m to have "-fopenmp" compiler option.

--- install.m
+++ install.m
@@ -1,5 +1,6 @@
-% Compile all Matlab mex files used in this project
+% Compile all Matlab mex/Octave oct files used in this project
 % You should have a compiler that support C++11
+% Here assume you are using GCC or a GCC compatible compiler
 
 GC_CAL_HOME = fileparts(mfilename('fullpath'));
 
@@ -15,23 +16,25 @@ end
 url_download_eigen = 'https://bitbucket.org/eigen/eigen/get/3.1.4.zip';
 dir_lib_ext = [GC_CAL_HOME,'/extern_lib/'];
 if ~exist([dir_lib_ext,'eigen3'], 'file')
-  fprintf('downloading and unziping Eigen...');  flushstdout();
-  %unzip(url_download_eigen,'extern_lib');  % Seems Matlab does not follow the redirect hint (HTTP 301) in the url.
-  urlwrite(url_download_eigen,'eigen-3.1.4.zip');
+  fprintf('Downloading Eigen...');  flushstdout();
+  urlwrite(url_download_eigen,'eigen-3.1.4.zip');  % Seems Matlab does not follow the redirect hint (HTTP 301) in the url.
+  fprintf('Unziping Eigen...');  flushstdout();
   unzip('eigen-3.1.4.zip','extern_lib');
+  % rename it, since I use #include <eigen3/***> in the cpp source code
   movefile([dir_lib_ext,'eigen-eigen-36bf2ceaf8f5'],[dir_lib_ext,'eigen3']);
   fprintf('done\n');  flushstdout();
 end
 
 disp('Compiling...');  flushstdout();
+
 cd([GC_CAL_HOME,'/GCcal']);
 if is_octave
   common_cmd_prefix = 'CXXFLAGS="-O3 -march=native -fopenmp -std=c++11"  LDFLAGS="-march=native -fopenmp" mkoctfile ';
   system([common_cmd_prefix,'--mex -o gendata_linear gendata_linear_v4_mex.cpp']);
   system([common_cmd_prefix,'--mex getcovzpdhded.cpp']);
-  system([common_cmd_prefix,'-o MAfilter MAfilter_v5.cpp']);
+  system([common_cmd_prefix,'MAfilter_v5.cpp -o MAfilter']);
 else
-  common_cmd_prefix = ['mex CXXFLAGS="\$CXXFLAGS -std=c++11 -I',dir_lib_ext,'" CXXOPTIMFLAGS="-O3 -march=native" '];
+  common_cmd_prefix = ['mex CXXFLAGS="\$CXXFLAGS -std=c++11 -I',dir_lib_ext,'" CXXOPTIMFLAGS="-O3 -march=native -fopenmp" LDFLAGS="\$LDFLAGS -fopenmp" '];
   eval([common_cmd_prefix, 'gendata_linear_v4_mex.cpp -output gendata_linear']);
   eval([common_cmd_prefix, 'getcovzpdhded.cpp']);
   eval([common_cmd_prefix, 'MAfilter_v5_mex.cpp -output MAfilter']);
--- startup.m
+++ startup.m
@@ -9,5 +9,49 @@ addpath([GC_CAL_HOME,'/tools_and_utilities']);
 addpath([GC_CAL_HOME,'/prj_neuron_gc']);
 addpath([GC_CAL_HOME,'/prj_neuron_gc/scan_worker_template']);
 
-disp('GC calculation packages loaded.');
+disp('GC calculation package path added.');
 disp(' ');
+
+% check whether the mex files are up to date
+cppfiles={
+'GCcal/gendata_linear_v4_mex.cpp'
+'GCcal/getcovzpdhded.cpp'
+'GCcal/MAfilter_v5_mex.cpp'
+'GCcal_spectrum/GaussianConvGrid.cpp'
+'GCcal_spectrum/lowest_smooth_number_exact.cpp'
+'GCcal_spectrum/lowest_smooth_number_fast.cpp'
+};
+mexfiles={
+'GCcal/gendata_linear'
+'GCcal/getcovzpdhded'
+'GCcal/MAfilter'
+'GCcal_spectrum/GaussianConvGrid'
+'GCcal_spectrum/lowest_smooth_number_exact'
+'GCcal_spectrum/lowest_smooth_number_fast'
+};
+for id_f=1:length(cppfiles)
+  path_cpp = [GC_CAL_HOME '/' cppfiles{id_f}];
+  path_mex = [GC_CAL_HOME '/' mexfiles{id_f} '.' mexext];
+  dcpp=dir(path_cpp);
+  dmex=dir(path_mex);
+  if isempty(dcpp)
+    disp(['GC package: "' path_cpp '" not found!']);
+    error('GC package: your copy of this package is broken.');
+    break;
+  end
+  if isempty(dmex)
+    if exist('OCTAVE_VERSION','builtin')
+      path_mex = [GC_CAL_HOME '/' mexfiles{id_f} '.oct'];
+      dmex=dir(path_mex);
+    end
+    if isempty(dmex)
+      disp(['GC package: "' path_mex '" not found!']);
+      error('GC package: run `install'' to generate all necessary mex files.');
+    end
+    break;
+  end
+  if dcpp.datenum > dmex.datenum
+    warning('GC package: some mex file(s) are not up to date, run `install'' to re-generate them.');
+    break;
+  end
+end
