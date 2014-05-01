406a4195ee1c1e929d94e8a4baff8af12647a261
xyy@xle <xyy@xle>
2014-05-01 16:57:54 +0800

    Trying better QR regression.

--- GCcal/WhiteningFilter.m
+++ GCcal/WhiteningFilter.m
@@ -1,12 +1,17 @@
 %% get residual and coefficients of self-regression
 
-function [srd, sas] = WhiteningFilter(X, use_od)
+function [srd, sas] = WhiteningFilter(X, use_od, coeff_mode)
 [p, len] = size(X);
+if exist('coeff_mode','var') && strcmpi(coeff_mode,'qr')
+    FitCoef = @(x) pos_nGrangerT_qr(x, use_od);
+else
+    FitCoef = @(x) pos_nGrangerT2(x, use_od);
+end
 
 srd = zeros(p, len);
 sas = zeros(p, use_od);
 for k=1:p
-    [~, ~, sa] = pos_nGrangerT2(X(k,:), use_od);
+    [~, ~, sa] = FitCoef(X(k,:));
     srd(k,:) = filter([1, sa], [1], X(k,:)-mean(X(k,:),2));
     sas(k,:) = sa;
 end
