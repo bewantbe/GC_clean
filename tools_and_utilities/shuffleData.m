c357053a566da550854e82c6f2c1e8e0f5a6cb72
xyy@xle <xyy@xle>
2014-10-30 18:51:25 +0800

    Correct shuffleData.m.

--- tools_and_utilities/shuffleData.m
+++ tools_and_utilities/shuffleData.m
@@ -3,9 +3,11 @@
 function X = shuffleData(X)
 [p,len] = size(X);
 
-%for k=1:p
-%  [~,dd] = sort(rand(1,len));
-%  X(k,:) = X(k,dd);
-%end
-X = X(randperm(len,p));
+for k=1:p
+  [~,dd] = sort(rand(1,len));
+  X(k,:) = X(k,dd);
+end
+%X = X'
+%X = X(randperm(len,p));
+%X = X';
 end
