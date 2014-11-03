07050abd0563dda460280902c4f72ee40aae45cb
xyy@xle <xyy@xle>
2014-11-03 21:46:56 +0800

    Make ARroots.m matlab compatible, and do not plot by default.

--- GCcal/ARroots.m
+++ GCcal/ARroots.m
@@ -1,19 +1,26 @@
 % Check the roots of AR process (confirm that AR is stable)
 
-function la = ARroots(A)
+function la = ARroots(A, b_show_plot)
 
 [p, mp] = size(A);
 B = zeros(mp, mp);
 B(1:p, :) = -A;
 B(p+1:mp+1:mp*(mp-p)) = 1;
 la = eig(B);
-la = la + 1e-300*I;            % convert all numbers to complex
-%compass(la);
-plot(la,'*', cos(0:0.01:2.01*pi), sin(0:0.01:2.01*pi));
-axis([-1.1,1.1,-1.1,1.1], 'square');
+if exist('b_show_plot','var') && b_show_plot
+  if isreal(la)
+    la = complex(la);
+  end
+  %compass(la);
+  plot(la,'*');
+  hold on
+  plot(cos(0:0.01:2.01*pi), sin(0:0.01:2.01*pi), 'r');
+  axis([-1.1,1.1,-1.1,1.1], 'square');
+  hold off
+end
 r = max(abs(la));
 if r >= 1
-    disp('Warning: unstable process !!');
+    warning('GC:AR:unstable', 'unstable process !!');
 end
 
 end
